b06ee9870bdf3b68976c57395e44b409e7dc7f16
Vítor F. Nery <vitorfelipenery@gmail.com>
2022-11-14 18:46:03 -0300

    initial commit

---
+++ TeamMemberAllocation/src/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  let today = new Date();
+
+  return (
+    <footer className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1>Team Member Allocation App - {today.getFullYear()}</h1>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
